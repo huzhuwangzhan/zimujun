4a2dbc6adb82a823e580e2382ab7b972d9886ec9
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-08-26 18:04:21 -0500

    added version control line to specify use py3 in jp_speech_recog.py

--- jp_speech_recognition.py
+++ jp_speech_recognition.py
@@ -1,8 +1,15 @@
+#!/anaconda/envs/py35/bin/python
+
 import subprocess
 
+INPUT = 'audio_inputs.txt'
+OUTPUT = 'output.txt'
 
+command = './bin/osx/julius -C main.jconf -C am-dnn.jconf -input rawfile -filelist '+ INPUT + ' -demo -dnnconf julius.dnnconf $*'
 
-command = './bin/osx/julius -C main.jconf -C am-dnn.jconf -input rawfile -filelist audio_inputs.txt -demo -dnnconf julius.dnnconf $*'
+p = subprocess.Popen(command, shell=True,stdout= subprocess.PIPE)
+out, err = p.communicate()
+out_decoded = out.decode('utf-8')
+out_result = out_decoded[out_decoded.find('sentence1:')+11:]
 
-p = subprocess.call(command, shell=True)
-
+print (out_result)
